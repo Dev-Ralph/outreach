aeeb0620c75bd96c11fc3558499d3952755009be
keith godoy <kkgodoy@mls.ceu.edu.ph>
2019-11-30 21:56:27 +0800

    change-hover-colors

--- participant.php
+++ participant.php
@@ -29,6 +29,7 @@ if(isset($username))
   <link href="vendor/fonts/css/fontawesome.min.css" rel="stylesheet">
   <link href="vendor/fonts/css/all.css" rel="stylesheet">
   <link rel="stylesheet" href="resource/css/animate.min.css">
+  <link href="resource/css/edit.css" rel="stylesheet">
   <link rel="icon" href="resource/img/icon-tab.png">
 </head>
 
